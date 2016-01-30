13133225293a9f1a7fa62be0a2c4117117e3a26d
SNT93 <sudeshnt93@gmail.com>
2016-01-29 19:45:01 -0800

    added rajitha's views , controllers , assets

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    public function home() {
+
+        return view('FrontEnd.Home');
+
+    }
+
+    public function test() {
+
+        return view('FrontEnd.test');
+
+    }
+
+}
